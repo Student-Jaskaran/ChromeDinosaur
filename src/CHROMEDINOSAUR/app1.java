abe7145c7e529e03cbb2e42647603bc992550e77
Student-Jaskaran <jaskaranrooprai23@gmail.com>
2025-07-09 13:08:52 +0530

    first commit

---
+++ src/CHROMEDINOSAUR/app1.java
@@ -0,0 +1,25 @@
+package CHROMEDINOSAUR;
+
+import javax.swing.*;
+
+public class app1
+{
+    public static void main(String[] args)
+    {
+        int boardWidth = 750;
+        int boardHeight = 250;
+
+        JFrame frame = new JFrame("Chrome Dinosaur");
+        //frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);// FOR center broad
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ChromeDinosaur chromeDinosaur = new ChromeDinosaur();
+        frame.add(chromeDinosaur);
+        frame.pack();
+        chromeDinosaur.requestFocus();
+        frame.setVisible(true);
+    }
+}
